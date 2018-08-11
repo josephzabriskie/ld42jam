dfc6704a80b68220c6fe1d6883b416ed1007694b
Joe Zabriskie <josephzabriskie@gmail.com>
2018-08-10 20:00:40 -0600

    Added Main Menu Scene
    Main menu skeleton. Needs to be filled in with better graphics

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+	public List<string> levelNames = new List<string>(new string[]{ "-", "JoeRezTest", "SampleScene" });
+	public string firstLevel = "JoeRezTest";
+	public Dropdown levelDropDown;
+
+	// Use this for initialization
+	void Start () {
+		this.levelDropDown.ClearOptions();
+		this.levelDropDown.AddOptions(this.levelNames);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartGame(){
+		this.LoadScene(this.firstLevel);
+	}
+
+	public void QuitGame(){
+#if UNITY_EDITOR
+	if(UnityEditor.EditorApplication.isPlaying) 
+	{
+		UnityEditor.EditorApplication.isPlaying = false;
+	}
+#endif
+			Application.Quit();
+	}
+
+	public void StartLevelSelect(){
+		if(this.levelDropDown.value == 0){
+			Debug.Log("Level 0 is reserved as a 'no choice' option, do nothing");
+			return;
+		}
+		Debug.Log("Starting Level: " + levelDropDown.value.ToString());
+		this.LoadScene(this.levelNames[levelDropDown.value]);
+	}
+
+	void LoadScene(string scene){
+		SceneManager.LoadScene(scene);
+	}
+}
