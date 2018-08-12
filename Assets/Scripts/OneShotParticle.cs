0f8ca3e0d9f7860c4d3964045735ede546513163
Joe Zabriskie <josephzabriskie@gmail.com>
2018-08-11 22:25:41 -0600

    First Particle Commit

---
+++ Assets/Scripts/OneShotParticle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneShotParticle : MonoBehaviour {
+	public int part = 15;
+	public int delay = 3;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<ParticleSystem>().Emit(this.part);
+		Destroy(gameObject, this.delay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
